8cfe3b5de7b9c19aac22f2ef702e37ecccfcd110
bouhass <bouhass@gmail.com>
2013-11-07 16:19:22 +0000

    - changed code coverage dependency from test to build - temp disabled most tests to test build with coverage

--- WardBook/test/unit/com/wardbook/TaskTests.groovy
+++ WardBook/test/unit/com/wardbook/TaskTests.groovy
@@ -1,19 +1,19 @@
-package com.wardbook
-
-
-
-import grails.test.mixin.*
-
-import java.util.concurrent.TimeUnit
-
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
-@TestFor(Task)
-class TaskTests {
-
-    void testToString() {
-        def task = new Task(name: 'task name', category: 'task type')
-        assert task.toString() == 'task name'
-    }
-}
+//package com.wardbook
+//
+//
+//
+//import grails.test.mixin.*
+//
+//import java.util.concurrent.TimeUnit
+//
+///**
+// * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+// */
+//@TestFor(Task)
+//class TaskTests {
+//
+//    void testToString() {
+//        def task = new Task(name: 'task name', category: 'task type')
+//        assert task.toString() == 'task name'
+//    }
+//}
--- WardBook/test/unit/com/wardbook/WardTests.groovy
+++ WardBook/test/unit/com/wardbook/WardTests.groovy
@@ -1,18 +1,18 @@
-package com.wardbook
-
-
-
-import grails.test.mixin.*
-import org.junit.*
-
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
-@TestFor(Ward)
-class WardTests {
-
-    void testToString() {
-        def ward = new Ward(name: 'paul wood', speciality: 'cardiology')
-        assert ward.toString() == 'paul wood - cardiology'
-    }
-}
+//package com.wardbook
+//
+//
+//
+//import grails.test.mixin.*
+//import org.junit.*
+//
+///**
+// * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+// */
+//@TestFor(Ward)
+//class WardTests {
+//
+//    void testToString() {
+//        def ward = new Ward(name: 'paul wood', speciality: 'cardiology')
+//        assert ward.toString() == 'paul wood - cardiology'
+//    }
+//}
